45463598ce3ef5cf64adbddc1b17ca486982fa6c
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-02-28 01:23:23 +0530

    logs assignee and taskDescription

--- commands/assign.js
+++ commands/assign.js
@@ -2,6 +2,14 @@ module.exports = {
 	name: 'assign',
 	description: 'Ping!',
 	execute(message, args) {
-		message.channel.send('Pong.');
+		// args splits the string into words by default. 
+		// assignee is the first word of the argument.
+		const assignee = args[0];
+
+		const taskDescription = args.join(' ').slice(assignee.length).trim();
+
+
+		console.log(assignee);
+		console.log(taskDescription);
 	},
 };
